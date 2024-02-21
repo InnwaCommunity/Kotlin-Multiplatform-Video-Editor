fea334fc40156b95b969eb8e064f84df94d53b94
nyein_chan_toe <nyeinchantoe0@gmail.com>
2024-02-21 14:16:34 +0630

    Add Destination Bar

--- app/src/main/java/com/manage/composesamples/model/SnackCollection.kt
+++ app/src/main/java/com/manage/composesamples/model/SnackCollection.kt
@@ -1,2 +1,13 @@
 package com.manage.composesamples.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class SnackCollection(
+    val id: Long,
+    val name: String,
+    val snacks: List<Snack>,
+    val type: CollectionType = CollectionType.Normal
+)
+
+enum class CollectionType { Normal, Highlight }
--- app/src/main/java/com/manage/composesamples/ui/home/Feed.kt
+++ app/src/main/java/com/manage/composesamples/ui/home/Feed.kt
@@ -1,8 +1,12 @@
 package com.manage.composesamples.ui.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.manage.composesamples.ui.components.JetsnackScaffold
+import com.manage.composesamples.ui.components.JetsnackSurface
 
 @Composable
 fun Feed(
@@ -10,5 +14,24 @@ fun Feed(
     onNavigateToRoute: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Text(text = "Hello World")
+    JetsnackScaffold(
+        modifier = modifier
+    ) {paddingValues ->
+        Feed(
+            onSnackClick,
+            Modifier.padding(paddingValues)
+        )
+    }
+}
+
+@Composable
+private fun Feed(
+    onSnackClick: (Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    JetsnackSurface(modifier = modifier.fillMaxSize()) {
+        Box {
+            DestinationBar()
+        }
+    }
 }
