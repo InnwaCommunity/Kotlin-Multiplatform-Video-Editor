971f841c739a4516e0224516d2b8dc0803acfbdf
nyein_chan_toe <nyeinchantoe0@gmail.com>
2024-02-27 21:38:04 +0630

    add main feed

--- androidApp/src/main/java/com/manage/videoeditor/android/composeui/MainFeed.kt
+++ androidApp/src/main/java/com/manage/videoeditor/android/composeui/MainFeed.kt
@@ -1,2 +1,15 @@
 package com.manage.videoeditor.android.composeui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import com.manage.videoeditor.app.FeedStore
+import com.manage.videoeditor.core.entity.Post
+
+@Composable
+fun MainFeed(
+    store: FeedStore,
+    onPostClick: (Post) -> Unit,
+    onEditClick: () -> Unit
+) {
+    val state = store.observeState().collectAsState()
+}
--- androidApp/src/main/java/com/manage/videoeditor/android/composeui/Screens.kt
+++ androidApp/src/main/java/com/manage/videoeditor/android/composeui/Screens.kt
@@ -1,10 +1,14 @@
 package com.manage.videoeditor.android.composeui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
+import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -24,6 +28,12 @@ class MainScreen : Screen, KoinComponent {
         val refreshState = rememberPullRefreshState(
             refreshing = state.progress,
             onRefresh = { store.dispatch(FeedAction.Refresh(true)) })
-    }
 
+        LaunchedEffect(Unit) {
+            store.dispatch(FeedAction.Refresh(false))
+        }
+        Box(modifier = Modifier.pullRefresh(refreshState)) {
+            MainF
+        }
+    }
 }
