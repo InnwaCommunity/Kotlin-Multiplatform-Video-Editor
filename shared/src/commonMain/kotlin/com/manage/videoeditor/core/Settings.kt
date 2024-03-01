7fb82e35397a9e145ced0c05083215a05fb26f5c
nyein_chan_toe <nyeinchantoe0@gmail.com>
2024-03-01 22:20:48 +0630

    set theme

--- shared/src/commonMain/kotlin/com/manage/videoeditor/core/Settings.kt
+++
@@ -1,5 +0,0 @@
-package com.manage.videoeditor.core
-
-class Settings(val defaultFeedUrls: Set<String>) {
-    fun isDefault(feedUrl: String) = defaultFeedUrls.contains(feedUrl)
-}
---
+++ shared/src/commonMain/kotlin/com/manage/videoeditor/domain/repositories/SettingsRepository.kt
@@ -0,0 +1,4 @@
+package com.manage.videoeditor.domain.repositories
+
+interface SettingsRepository {
+}
